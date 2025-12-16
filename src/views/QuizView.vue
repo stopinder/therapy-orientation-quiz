2d2b0ecd36cf3a565c93df65ac7263720f1bca28
Robert Ormiston <emdrifs@robormiston.com>
2025-12-16 07:23:23 +0700

    Fix AI expand payload key mismatch

--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -198,9 +198,9 @@ async function expandWithAI() {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
-        deterministicReport: report.value
-
+        report: report.value
       })
+
     })
 
     const data = await res.json()
