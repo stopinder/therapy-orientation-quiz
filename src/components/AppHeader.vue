7fc9e17e8d3153829848ed86aef960bb97f5b835
Robert Ormiston <emdrifs@robormiston.com>
2025-12-19 18:23:38 +0700

    Improve nav item differentiation and spacing

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,23 +4,30 @@
       <div class="flex items-center">
 
         <!-- Brand -->
-        <div class="text-lg font-semibold tracking-tight text-slate-900">
-          MindWorks
+        <div class="text-xl font-semibold tracking-tight text-slate-900">
+
+        MindWorks
         </div>
 
 
         <!-- Navigation -->
-        <nav class="ml-auto flex items-center gap-2 text-sm font-medium text-slate-700">
-          <router-link to="/about" class="hover:text-slate-900 transition">
+        <nav class="ml-auto flex items-center gap-6 text-base font-medium text-slate-700">
+
+
+          <router-link
+              to="/about"
+              class="hover:text-slate-900 transition"
+          >
             About
           </router-link>
 
           <a
               href="https://gpttherapyassist.lemonsqueezy.com/buy/519426ee-40c5-4fd2-930a-fba4fdd6bf0d?redirect_url=https://therapy-orientation-quiz.vercel.app/gateway?order_id={order_id}"
-              class="hover:text-slate-900 transition"
+              class="hover:text-slate-900 transition text-slate-800"
           >
             ADHD Quiz
           </a>
+
         </nav>
 
       </div>
