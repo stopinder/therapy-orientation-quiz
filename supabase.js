f829b4671fadaa0e64426db2709d750aa7c13356
Robert Ormiston <emdrifs@robormiston.com>
2026-05-09 17:44:37 +0200

    Rebuild Supabase client initialization

--- supabase.js
+++ supabase.js
@@ -1,7 +1,10 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js"
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+
+console.log("SUPABASE URL:", supabaseUrl)
+console.log("SUPABASE KEY:", supabaseAnonKey)
 
 export const supabase = createClient(
     supabaseUrl,
