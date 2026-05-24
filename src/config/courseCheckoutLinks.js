9aa1bab2a9d59a33db9ecf9b3e808a1ff14ea45f
Robert Ormiston <emdrifs@robormiston.com>
2026-05-24 09:21:04 +0200

    Finalize sequential Lemon Squeezy checkout mapping for weekly programme structure

--- src/config/courseCheckoutLinks.js
+++ src/config/courseCheckoutLinks.js
@@ -6,22 +6,22 @@ export const COURSE_CHECKOUT_LINKS = {
     weekly: {
 
         1:
-            "https://gpttherapyassist.lemonsqueezy.com/checkout/buy/47796659-f7c8-4807-b655-a6be834e3125",
+            "https://gpttherapyassist.lemonsqueezy.com/checkout/buy/4cc00190-4bf8-4bad-a904-d877c8b9b543",
 
         2:
-            "https://gpttherapyassist.lemonsqueezy.com/checkout/buy/bc44034b-9bef-4c4f-8acd-49eee20e3763?discount=0",
+            "https://gpttherapyassist.lemonsqueezy.com/checkout/buy/e9aab155-2833-41ae-a971-5dea04007309",
 
         3:
-            "https://gpttherapyassist.lemonsqueezy.com/checkout/buy/d21b6b4a-8ed7-4bd9-bad1-25f4de7e24fe?discount=0",
+            "https://gpttherapyassist.lemonsqueezy.com/checkout/buy/d21b6b4a-8ed7-4bd9-bad1-25f4de7e24fe",
 
         4:
-            "https://gpttherapyassist.lemonsqueezy.com/checkout/buy/df02503b-f965-4edb-a8b9-1e8da44f5d30",
+            "https://gpttherapyassist.lemonsqueezy.com/checkout/buy/017bc42d-8745-418d-8ded-9b21f1df3c0e",
 
         5:
-            "https://gpttherapyassist.lemonsqueezy.com/checkout/buy/a886c2ca-914f-435a-8eb3-9f2bf476f9d6",
+            "https://gpttherapyassist.lemonsqueezy.com/checkout/buy/fe792848-eca3-4079-a939-11d7ffac0a3a",
 
         6:
-            "https://gpttherapyassist.lemonsqueezy.com/checkout/buy/ac5f9bd1-8881-41bb-9898-8bb7fe790849"
+            "https://gpttherapyassist.lemonsqueezy.com/checkout/buy/8a0dba72-8947-4fef-ad34-52a8da3f5117"
 
     }
 
